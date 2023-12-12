575506ff82d8b3173cb548f0dd85bec0e2066fc5
mertkuleci <mert22kuleci@gmail.com>
2023-12-12 18:24:57 +0300

    Proje dosyası ekle.

---
+++ server_408/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace client_408
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(198, 48);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(167, 27);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(198, 84);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(167, 27);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port Number:";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(199, 124);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(166, 27);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Username:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 69);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Connect to the Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(723, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 49);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Subscribe to SPS101 Server";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(845, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 104);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Disconnect";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Send Message To IF 100 Server:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(642, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(219, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Send Message To SPS 101 Server:";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(58, 220);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(307, 30);
+            this.richTextBox4.TabIndex = 11;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(527, 223);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(346, 26);
+            this.richTextBox5.TabIndex = 12;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(58, 296);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(431, 129);
+            this.richTextBox6.TabIndex = 13;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(527, 294);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(463, 131);
+            this.richTextBox7.TabIndex = 14;
+            this.richTextBox7.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(221, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "IF100";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(720, 271);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "SPS101";
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.Location = new System.Drawing.Point(66, 483);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.Size = new System.Drawing.Size(923, 87);
+            this.richTextBox8.TabIndex = 17;
+            this.richTextBox8.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(483, 454);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Status";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(597, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 49);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Subscribe to IF100 Server";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(597, 94);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 49);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Unsubscribe to IF100 Server";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(723, 94);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 49);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "Unsubscribe to SPS101 Server";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(383, 221);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(95, 27);
+            this.button7.TabIndex = 22;
+            this.button7.Text = "SEND";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(899, 220);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(90, 29);
+            this.button8.TabIndex = 23;
+            this.button8.Text = "SEND";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 584);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ server_408/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace client_408
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient tcpClient;
+        private NetworkStream clientStream;
+        private Thread receiveThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectToServer(string serverIp, int serverPort, string username)
+        {
+            try
+            {
+                tcpClient = new TcpClient(serverIp, serverPort);
+                clientStream = tcpClient.GetStream();
+
+                // Send the username to the server
+                SendMessage($"CONNECT|{username}");
+
+                // Start a thread to listen for messages from the server
+                receiveThread = new Thread(new ThreadStart(ReceiveMessages));
+                receiveThread.Start();
+
+                richTextBox6.AppendText($"Connected to server {serverIp}:{serverPort} as {username}\n");
+            }
+            catch (Exception ex)
+            {
+                richTextBox6.AppendText($"Error connecting to server: {ex.Message}\n");
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] message = new byte[4096];
+                    int bytesRead = clientStream.Read(message, 0, 4096);
+
+                    if (bytesRead == 0)
+                        break;
+
+                    string data = Encoding.ASCII.GetString(message, 0, bytesRead);
+                    ProcessServerMessage(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox6.AppendText($"Error receiving message from server: {ex.Message}\n");
+            }
+        }
+
+        private void ProcessServerMessage(string message)
+        {
+            // Implement your logic to handle different types of server messages
+            // For example, you can split the message into parts and switch based on the action
+            string[] parts = message.Split('|');
+
+            if (parts.Length >= 2)
+            {
+                string action = parts[0].ToUpper();
+                string channel = parts[1].ToUpper();
+
+                switch (action)
+                {
+                    case "MESSAGE":
+                        if (parts.Length >= 4)
+                        {
+                            string sender = parts[2];
+                            string data = parts[3];
+                            DisplayMessage(channel, sender, data);
+                        }
+                        break;
+                        // Add cases for other server actions if needed
+                }
+            }
+            else
+            {
+                richTextBox6.AppendText($"Invalid message format from server: {message}\n");
+            }
+        }
+
+        private void DisplayMessage(string channel, string sender, string data)
+        {
+            // Implement how you want to display the message in the client GUI
+            richTextBox1.AppendText($"[{channel}] {sender}: {data}\n");
+        }
+
+        private void SendMessage(string message)
+        {
+            try
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(message);
+                clientStream.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                richTextBox6.AppendText($"Error sending message to server: {ex.Message}\n");
+            }
+        }
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string message = richTextBox4.Text;
+            SendMessage($"SEND|IF100|{message}");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string message = richTextBox5.Text;
+            SendMessage($"SEND|SPS101|{message}");
+        }
+   
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Clean up resources when the form is closing
+            if (tcpClient != null)
+            {
+                tcpClient.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                string serverIp = richTextBox1.Text;
+                int serverPort;
+
+                if (int.TryParse(richTextBox2.Text, out serverPort))
+                {
+                    string username = richTextBox3.Text;
+                    ConnectToServer(serverIp, serverPort, username);
+                }
+                else
+                {
+                    richTextBox6.AppendText("Invalid server port number.\n");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Implement how to subscribe to channels
+            // You may need to modify this based on your specific design
+            SendMessage("SUBSCRIBE|IF100");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Implement how to subscribe to channels
+            // You may need to modify this based on your specific design
+            SendMessage("SUBSCRIBE|SPS101");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Implement how to subscribe to channels
+            // You may need to modify this based on your specific design
+            SendMessage("UNSUBSCRIBE|IF100");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Implement how to subscribe to channels
+            // You may need to modify this based on your specific design
+            SendMessage("UNSUBSCRIBE|SPS101");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Implement how to subscribe to channels
+            // You may need to modify this based on your specific design
+            SendMessage("DISCONNECT");
+        }
+
+    }
+}
+
